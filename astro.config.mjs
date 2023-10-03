0819f0b682369aef3370479e9134ad029fc83a8a
Eduardo Higareda <eldelentes@gmail.com>
2023-10-03 11:02:57 -0600

    Add aditional data

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://briqmx.github.io',
-  base: '/space-landing-begrand',
+  // base: '/space-landing-begrand',
+  base: '/',
   integrations: [tailwind(), icon({
     iconDir: "./src/icons",
     include: {
