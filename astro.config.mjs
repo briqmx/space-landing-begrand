38b881683c1df9060f1849e72a49d0cdd148c598
Eduardo Higareda <eldelentes@gmail.com>
2023-10-03 11:05:42 -0600

    Fix base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://briqmx.github.io',
-  // base: '/space-landing-begrand',
-  base: '/',
+  base: '/space-landing-begrand',
+  // base: '/',
   integrations: [tailwind(), icon({
     iconDir: "./src/icons",
     include: {
