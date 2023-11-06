09b3aa2f7de99a3f748307bf77b7cc46b7340323
Eduardo Higareda <eldelentes@gmail.com>
2023-11-06 11:10:19 -0600

    improved logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://briqmx.github.io',
+  site: 'https://briqmx.github.io',
   base: '/space-landing-begrand',
   // base: '/',
   integrations: [tailwind(), icon({
