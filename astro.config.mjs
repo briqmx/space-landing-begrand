7040ae052109754c70832e96bda06c701afd51fc
Eduardo Higareda <eldelentes@gmail.com>
2023-11-07 16:58:44 -0600

    Fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,15 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from '@astrojs/react';
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://briqmx.github.io',
-  base: '/space-landing-begrand',
+  // base: '/space-landing-begrand',
   // base: '/',
+  base: isDevelopment ? '/' : '/space-landing-begrand/',
   integrations: [tailwind(), icon({
     iconDir: "./src/icons",
     include: {
