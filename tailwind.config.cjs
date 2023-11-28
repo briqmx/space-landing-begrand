0ac6ad79da94e7762573b7d9ea77542ebec81c86
Eduardo Higareda <eldelentes@gmail.com>
2023-11-28 16:47:39 -0600

    Add version two

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,12 @@ module.exports = {
 			colors: {
 				primary: '#1AA2E6',
 				navy: '#041c2c',
+				beige: "#f9f9f6",
+				'beige-dark': "#f5f1e9"
 			},
 			fontFamily: {
 				display: ['Archia', 'sans-serif'],
+				body: ['Instrument Sans', 'sans-serif'],
 			}
 		},
 	},
