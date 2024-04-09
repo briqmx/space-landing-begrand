0624cc2c22bc39c9928370c3553ae7747a3309f3
Eduardo <eldelentes@gmail.com>
2024-04-09 12:52:23 -0600

    minor fixes

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react';
 
 const Calculator = () => {
-  const [meters, setMeters] = useState(2);
-  const [years, setYears] = useState(10);
-  const [price, setPrice] = useState(7200000);
-  const [appreciation, setAppreciation] = useState(6.6);
-  const [rentPerYear, setRentPerYear] = useState(30000);
+  const [meters, setMeters] = useState(1);
+  const [years, setYears] = useState(15);
+  const [price, setPrice] = useState(7125000);
+  const [appreciation, setAppreciation] = useState(8.0);
+  const [inflation, setInflation] = useState(4.0);
+  const [rentPerMonth, setRentPerMonth] = useState(30000);
   const [appreciationPerYear, setAppreciationPerYear] = useState([]);
   const [viewDetail, setViewDetail] = useState(false);
 
-  const rentPerTerm = (rentPerYear *  meters) * years;
+  const rentPerTerm = (rentPerMonth * (((Math.pow(1 + inflation / 12 / 100, years * 12) - 1) / (inflation / 12 / 100)) * (1 + inflation / 12 / 100)) * meters);
   const totalEarnings = price * meters * (1 + appreciation / 100) ** years;
 
   const centsToCurrency = (cents) => {
@@ -57,27 +58,27 @@ const Calculator = () => {
   return (
     <div className="gap-4 md:gap-8 grid grid-cols-1 md:grid-cols-2">
       <div className="flex flex-col items-start gap-8">
-        <img src="/logo-nav.png" alt="Logo" class="w-auto h-5 md:h-8" />
+        <img src="/logo-nav.png" alt="Logo" className="w-auto h-5 md:h-8" />
         <div>
-          <h1 className="text-xl md:text-2xl">Conoce cuanto puede crecer tu patrimonio si inviertes en <strong>Vitant del Valle</strong></h1>
+          <h1 className="text-xl md:text-2xl">Conoce cuanto puede crecer tu patrimonio si inviertes en <strong>Vitant® Del Valle</strong></h1>
         </div>
-        <div className="flex flex-col gap-4">
-          <p className="text-balance">Indica cuantos metros cuadrados quieres comprar de este proyecto:</p>
-          <div className="flex items-center gap-4">
+        <div className="flex flex-col gap-4 w-full">
+          <p className="text-balance text-sm md:text-lg">¿Cuántos m2 quieres comprar?</p>
+          <div className="flex items-center gap-4 w-full">
             <button className="flex justify-center items-center bg-white border rounded-full size-10" onClick={decrementMeters}>
               -
             </button>
             <div className="flex items-center gap-1 text-2xl">
               <input type="number" className="text-right bg-transparent w-[3ch] font-bold" value={meters} readOnly />
-              <span>m2</span>
+              <span>m²</span>
             </div>
             <button className="flex justify-center items-center bg-white border rounded-full size-10"   onClick={incrementMeters}>
               +
             </button>
           </div>
         </div>
-        <div className="flex flex-col gap-4">
-          <p className="text-balance">Indica por cuantos años quisieras conservar tus metros cuadrados:</p>
+        <div className="flex flex-col gap-4 w-full">
+          <p className="text-balance text-sm md:text-lg">¿Por cuántos años los quisieras conservar?</p>
           <div className="flex items-center gap-4">
             <button className="flex justify-center items-center bg-white border rounded-full size-10" onClick={decrementYears}>
               -
@@ -91,21 +92,21 @@ const Calculator = () => {
             </button>
           </div>
         </div>
-        <div className="py-4 border-t">
-          <p>Por estos metros pagarías:</p>
+        <div className="py-4 border-t w-full">
+          <p>Por estos m² pagarías:</p>
           <strong>{centsToCurrency(price*meters)} pesos mexicanos</strong>
         </div>
       </div>
-      <div className="flex flex-col gap-8 bg-white shadow-sm p-8 border">
-        <div className="flex flex-col gap-2">
-          <p>
+      <div className="flex flex-col gap-8 bg-white shadow-sm p-4 md:p-8 border">
+        <div className="flex flex-col gap-4">
+          <p className="text-base md:text-lg">
             Con una plusvalía aproximada del <strong>{appreciation}%</strong> anual,
             tus metros valdrían en <strong>{years}</strong> años:</p>
           <div>
             <p className="font-semibold text-3xl">
               <span>{centsToCurrency(totalEarnings)} pesos</span>
             </p>
-            <p className="text-primary">Un incremento de {centsToCurrency(totalEarnings - price*meters)} pesos</p>
+            <p className="text-base text-primary md:text-lg">Un incremento de {centsToCurrency(totalEarnings - price*meters)} pesos</p>
           </div>
           <button className="text-left text-primary text-sm underline" onClick={() => setViewDetail(!viewDetail)}>
             {viewDetail ? 'Ocultar detalle' : 'Ver detalle'}
@@ -115,7 +116,7 @@ const Calculator = () => {
               <table className="border-collapse border w-full">
                 <thead className="bg-beige border-b text-sm">
                   <th className="p-2 text-left">Año</th>
-                  <th className="text-right p-2">Precio m2</th>
+                  <th className="text-right p-2">Precio m²</th>
                   <th className="text-right p-2">Plusvalía</th>
                 </thead>
                 <tbody className="font-mono text-sm">
@@ -132,19 +133,27 @@ const Calculator = () => {
           )}
         </div>
         <div className="flex flex-col gap-2">
-          <p>Y ganarías en rentas en 10 años:</p>
+          <p className="text-base md:text-lg">Y ganarías en rentas en 10 años:</p>
           <div>
             <p className="font-semibold text-3xl">{centsToCurrency(rentPerTerm)} pesos</p>
-            <p className="text-primary">{centsToCurrency(rentPerYear*meters)} de renta anual.</p>
+            <p className="text-base text-primary md:text-lg">{centsToCurrency((rentPerTerm/12))} de renta anual.</p>
           </div>
         </div>
-        <div className="flex flex-col gap-2 text-sm">
-          <p>✅ Además en todo momento eres dueño de tu m2</p>
-          <p>✅ Puedes comprar más m2 y canjearlos por otros proyectos de BeGrand</p>
+        <div className="flex flex-col gap-2 bg-beige border divide-y text-base">
+          <p className='p-2'>En total ganarias: {centsToCurrency((totalEarnings - price*meters) + rentPerTerm)} pesos</p>
+          <p className='p-2'>en todo momento eres dueño de tus m²</p>
+          <p className='p-2'>Puedes comprar más m² y canjearlos por otros proyectos de BeGrand</p>
         </div>
         <div className="flex flex-col gap-4">
           <a className="flex justify-center items-center gap-1 bg-primary p-3 border text-white" href="#">Comenzar a invertir</a>
+          <div className='flex justify-center items-center gap-4'>
+            <div className='bg-black/10 w-full h-px'></div>
+            <span>o</span>
+            <div className='bg-black/10 w-full h-px'></div>
+          </div>
+          <a className="flex justify-center items-center gap-1 bg-white p-3 border text-primary" href="#">Hablar con un asesor</a>
         </div>
+        <small>Para el cálculo de renta se considera una estimación de la inflación promedio del 4% por año</small>
       </div>
     </div>
   );
