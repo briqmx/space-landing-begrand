3fd4fdc045a269d6e3067ae0b15e3a756d870abd
Eduardo <eldelentes@gmail.com>
2024-06-26 18:30:19 -0600

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sumametros.briq.mx',
+  site: 'https://sumametros.mx',
   // base: '/space-landing-begrand',
   // base: '/',
   base: isDevelopment ? '/' : '/',
