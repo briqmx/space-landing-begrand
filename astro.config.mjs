7c478dac9f8d934fc38281ffa05cb63f2d7b6460
Eduardo <eldelentes@gmail.com>
2024-06-27 12:09:12 -0600

    change cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,10 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sumametros.briq.mx',
+  site: 'http://sumametros.mx',
   // base: '/space-landing-begrand',
   // base: '/',
   base: isDevelopment ? '/' : '/',
-  redirects: {
-    '/v2': '/',
-  },
   integrations: [tailwind(), icon({
     iconDir: "./src/icons",
     include: {
