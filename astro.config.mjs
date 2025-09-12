a89c304edff1c52a03687b990e00a077dfe7e78b
Eduardo Higareda <eldelentes@gmail.com>
2025-09-12 14:25:04 -0600

    add new design version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -10,14 +11,20 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 // https://astro.build/config
 export default defineConfig({
   site: 'http://sumametros.mx',
+
   // base: '/space-landing-begrand',
   // base: '/',
   base: isDevelopment ? '/' : '/',
-  integrations: [tailwind(), icon({
+
+  integrations: [icon({
     iconDir: "./src/icons",
     include: {
       bxl: ["*"],
       tabler: ["*"],
     }
   }),react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
